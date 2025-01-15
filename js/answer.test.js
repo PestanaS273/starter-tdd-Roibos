bcdcbd9a102317b9028ec48d2545be369a27c991
Lucas Brancolini <lucas.brancolini@efrei.net>
2025-01-15 10:39:54 +0100

    writted test depending on orientation

--- js/answer.test.js
+++ js/answer.test.js
@@ -1,10 +1,31 @@
 const { Rover, Position } = require('./answer')
 
-describe('Rover moving forward', () => {
-  test('it should have position y incremented', () => {
-    const rover = new Rover(0, 0);
+describe('Rover moving forward facing all directions', () => {
+  test('it should have position y incremented when facing North', () => {
+    const rover = new Rover(0, 0, 'N');
     rover.moveForward();
     expect(rover.position.y).toBe(1);
     expect(rover.position.x).toBe(0);
+  })
+
+  test('it should have position y decremented when facing South', () => {
+    const rover = new Rover(0, 0, 'S');
+    rover.moveForward();
+    expect(rover.position.y).toBe(-1);
+    expect(rover.position.x).toBe(0);
+  })
+
+  test('it should have position x incremented when facing East', () => {
+    const rover = new Rover(0, 0, 'E');
+    rover.moveForward();
+    expect(rover.position.y).toBe(0);
+    expect(rover.position.x).toBe(1);
+  })
+
+  test('it should have position x decremented when facing West', () => {
+    const rover = new Rover(0, 0, 'W');
+    rover.moveForward();
+    expect(rover.position.y).toBe(0);
+    expect(rover.position.x).toBe(-1);
   })
 })
